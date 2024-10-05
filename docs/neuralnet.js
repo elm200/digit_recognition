f97c8f36c267388171c3d97a11dc96991f9fc434
Eiji Sakai <eiji.sakai@enechange.co.jp>
2024-10-05 12:58:56 +0900

    Apply prettier

--- docs/neuralnet.js
+++ docs/neuralnet.js
@@ -21,9 +21,9 @@ const predict = (x) => {
 };
 
 const clear = () => {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'white';
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "white";
   ctx.fillRect(0, 0, 224, 224);
 };
 
@@ -31,36 +31,37 @@ const dupArrayLike = (arrayLike) => Array.from(arrayLike);
 
 const drawProbs = (y) => {
   for (let i = 0; i < 10; i++) {
-    document.getElementById(`prob${i}`).textContent = `${i}: ${(y.get(i) * 100).toFixed(2)}%`;
+    document.getElementById(`prob${i}`).textContent =
+      `${i}: ${(y.get(i) * 100).toFixed(2)}%`;
   }
 };
 
 const recognize = () => {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
   const image = ctx.getImageData(0, 0, 224, 224).data;
   const img = nj.array(dupArrayLike(image)).reshape(224, 224, 4);
   const resizedImg = nj.images.resize(img, 28, 28);
   const grayImg = nj.images.rgb2gray(resizedImg);
   const invertedImg = nj.ones(grayImg.shape).multiply(255).subtract(grayImg);
-  const transformed = document.getElementById('transformed');
+  const transformed = document.getElementById("transformed");
   nj.images.save(invertedImg, transformed);
   const x = nj.divide(invertedImg, 255.0).reshape(28 * 28);
   const y = predict(x);
   drawProbs(y);
   const a = argmax(y);
-  document.getElementById('result').textContent = a;
+  document.getElementById("result").textContent = a;
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('btn_recognize').addEventListener('click', recognize);
-  document.getElementById('btn_clear').addEventListener('click', clear);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn_recognize").addEventListener("click", recognize);
+  document.getElementById("btn_clear").addEventListener("click", clear);
 
-  const canvas = document.getElementById('canvas');
-  canvas.addEventListener('mousemove', (e) => {
+  const canvas = document.getElementById("canvas");
+  canvas.addEventListener("mousemove", (e) => {
     if (e.buttons === 1) {
-      const ctx = canvas.getContext('2d');
-      ctx.fillStyle = 'black';
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = "black";
       ctx.fillRect(e.offsetX, e.offsetY, 12, 12);
     }
   });
